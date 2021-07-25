f8729232588dbbcb8d4c9ba26d1f8750d68a51e2
taem1n1 <youman7953@naver.com>
2021-07-25 20:30:25 +0900

    실행 결과 확인용 Application

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -1,2 +1,20 @@
-package hello.core;public class MemberApp {
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "taemin", Grade.VIP); //id값에 L을 붙혀준 이유는 Long타입이기 떄문이다 L을 붙히지 않으면 오류가 난다.
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+
+    }
 }
