d4bfb35cb49d4dfe0d9efcf3849ad582798a25df
taem1n1 <youman7953@naver.com>
2021-08-23 09:01:25 +0900

    스프링 빈 조회 - 기본

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -22,11 +22,14 @@ public class ApplicationContextInfoTest {
 
     @Test
     @DisplayName("Application 빈 출력하기")
-    void  findApplicationBean() {
+    void  ffindApplicationBean() {
         String[] beanDifinitionNames = ac.getBeanDefinitionNames();
         for (String beanDifinitionName : beanDifinitionNames) {
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDifinitionName);
 
+            // ROLE_APPLICATION : 사용자가 직접 등록한 Bean 을 식별하기 위한 Role
+            // ROLE_INFRASTRUCTURE : Spring이 직접 등록한 Bean 을 식별하기 위한 Role
+            // NoSuchBeanDefinitionException : 조회 대상 Spring Bean이 없으면 터지는 Exception.
             if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
                 Object bean = ac.getBean(beanDifinitionName);
                 System.out.println("name = " + beanDifinitionName + "object = " + bean);
