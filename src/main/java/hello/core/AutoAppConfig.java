2bb9932eb6097cb4cdd218a1d971ca670e504093
taem1n1 <youman7953@naver.com>
2021-08-31 08:13:28 +0900

    중복 등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,6 +13,9 @@ import org.springframework.context.annotation.FilterType;
 //        basePackageClasses = AutoAppConfig.class,  따로 설정하지 않고 root 디렉터리에 두는것을 스프링 부트에서 권장한다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
-public class AutoAppConfig  {
-
+public class AutoAppConfig {
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memoryMemberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
